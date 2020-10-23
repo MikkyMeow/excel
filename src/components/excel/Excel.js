1a616cbbd2335648b0607758af359431459502aa
Artur Akhmadullin <arturakk2015@gmail.com>
2020-10-23 10:54:16 +0300

    replace html to scripts

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,10 +1,25 @@
+import { $ } from "../../core/dom"
+
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
   }
 
+  getRoot() {
+    const $root = $.create('div', 'excel')
+    
+    this.components.forEach(Component => {
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.innerHTML = component.toHTML()
+      $root.append($el)
+    });
+
+    return $root
+  }
+
   render() {
-    console.log('profit', this.$el)
+    this.$el.append(this.getRoot())
   }
 }

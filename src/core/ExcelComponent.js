e35ce545140180b0c3ae0a98ae7046987dee66bd
MikkyMeow <arturakk2015@gmail.com>
2021-03-30 10:34:09 +0300

    added directory

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,8 @@
+import { DomListener } from '@core/DOMListener';
+
+export class ExcelComponent extends DomListener {
+  // Возвращает шаблон компонента
+  toHTML() {
+    return '';
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { Excel } from './components/excel/Excel'
-import { Header } from './components/header/Header'
-import { Toolbar } from './components/toolbar/Toolbar'
-import { Formula } from './components/formula/Formula'
-import { Table } from './components/table/Table'
-import './scss/index.scss'
+import { Excel } from '@/components/excel/Excel';
+import './scss/index.scss';
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
-})
+  component: [],
+});
 
-excel.render()
+console.log('excel', excel);

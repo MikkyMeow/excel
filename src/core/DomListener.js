0b02028ef13562fd238b6a7a2f9626034b9d6206
MikkyMeow <arturakk2015@gmail.com>
2021-04-12 18:57:54 +0300

    added event listeners for Formula Component

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -9,10 +11,23 @@ export class DomListener {
 
   initDOMListeners() {
     this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(
+          `Method ${method} is not implemented in ${this.name} Component`
+        );
+      }
       // То же самое что и addEventListener
-      this.$root.on(listener, () => {});
+      this.$root.on(listener, this[method].bind(this));
     });
   }
 
-  removeDOMListeners() {}
+  removeDOMListeners() {
+    // todo
+  }
+}
+
+// input => onInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,6 +3,7 @@ import { DomListener } from '@core/DOMListener';
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
+    this.name = options.name || '';
   }
 
   // Возвращает шаблон компонента

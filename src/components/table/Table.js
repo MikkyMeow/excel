b0006c2ac231a77469a36c903826b53ebbb01115
MikkyMeow <arturakk2015@gmail.com>
2021-04-17 22:30:50 +0300

    added minimal breakpoints

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -12,7 +12,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(1550);
+    return createTable(50);
   }
 
   onMousedown(event) {
@@ -21,23 +21,33 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
       const type = $resizer.$el.dataset.resize;
+      const minWidth = 40;
+      const minHeight = 20;
       let value;
       const sideProp = type === 'col' ? 'bottom' : 'right';
 
       $resizer.css({
         opacity: 1,
-        [sideProp]: '-100vh',
+        [sideProp]: '-5000px',
       });
 
       document.onmousemove = e => {
         if (type === 'col') {
           const delta = e.pageX - coords.right;
           value = coords.width + delta;
-          $resizer.css({ right: -delta + 'px' });
+          if (value > minWidth) {
+            $resizer.css({ right: -delta + 'px' });
+          } else {
+            $resizer.css({ right: `calc(100% - ${minWidth})` });
+          }
         } else {
           const delta = e.pageY - coords.bottom;
           value = coords.height + delta;
-          $resizer.css({ bottom: -delta + 'px' });
+          if (value > minHeight) {
+            $resizer.css({ bottom: -delta + 'px' });
+          } else {
+            $resizer.css({ bottom: `calc(100% - ${minHeight})` });
+          }
         }
       };
 
@@ -46,11 +56,18 @@ export class Table extends ExcelComponent {
         document.onmouseup = null;
 
         if (type === 'col') {
+          if (value < minWidth) {
+            value = minWidth;
+          }
+
           $parent.css({ width: value + 'px' });
           this.$root
             .findAll(`[data-col="${$parent.data.col}"]`)
             .forEach(el => (el.style.width = value + 'px'));
         } else {
+          if (value < minHeight) {
+            value = minHeight;
+          }
           $parent.css({ height: value + 'px' });
         }
 

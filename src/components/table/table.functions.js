70b7a0110732f32cdd56593b793760b3003418e4
MikkyMeow <arturakk2015@gmail.com>
2021-04-19 12:16:37 +0300

    added key navigation

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -20,3 +20,26 @@ export function matrix($target, $current) {
     return acc;
   }, []);
 }
+
+export function nextSelector(key, { col, row }) {
+  const MIN_VALUE = 0;
+
+  switch (key) {
+    case 'Enter':
+    case 'ArrowDown':
+      row++;
+      break;
+    case 'Tab':
+    case 'ArrowRight':
+      col++;
+      break;
+    case 'ArrowLeft':
+      col = col === MIN_VALUE ? col : col - 1;
+      break;
+    case 'ArrowUp':
+      row = row === MIN_VALUE ? row : row - 1;
+      break;
+  }
+
+  return `[data-id="${row}:${col}"]`;
+}
--- src/core/dom.js
+++ src/core/dom.js
@@ -77,6 +77,11 @@ class Dom {
     return this.data.id;
   }
 
+  focus() {
+    this.$el.focus();
+    return this;
+  }
+
   addClass(className) {
     return this.$el.classList.add(className);
   }

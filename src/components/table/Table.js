048b2a4c3bdcea1843e680231994326f86b9e7e5
MikkyMeow <arturakk2015@gmail.com>
2021-04-26 18:21:34 +0300

    added localStorage hook

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
 import { isCell, shouldResize, matrix, nextSelector } from './table.functions';
 import { TableSelection } from './TableSelection';
+import * as actions from '@/redux/actions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -36,10 +37,6 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus();
     });
-
-    this.$subscribe(state => {
-      console.log('table state ', state);
-    });
   }
 
   selectCell($cell) {
@@ -50,7 +47,7 @@ export class Table extends ExcelComponent {
   async resizeTable(event) {
     try {
       const data = await resizeHandler(this.$root, event);
-      this.$dispatch({ type: 'TABLE_RESIZE', data });
+      this.$dispatch(actions.tableResize(data));
     } catch (e) {
       console.warn('Resize error', e.message);
     }
